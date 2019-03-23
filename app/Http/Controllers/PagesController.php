38446c74817551b0e77a8b9adfe107e973ccf9c7
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-23 17:09:50 +0100

    package

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use SMTPValidateEmail\SMTPValidateEmail;
+
 class PagesController extends Controller
 {
+
     public function home(){
 
-        return "Foo";
+
+        $from = 'sendingemail@test.test';
+        $email = 'emailtoverify@test.test';
+
+        $validator = new SMTPValidateEmail($email, $from);
+        $smtp_results = $validator->validate();
+
+        print_r($smtp_results);
+
     }
 }
