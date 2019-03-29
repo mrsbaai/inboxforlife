4c554ae324762407c488806d032eb80cd9167157
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-29 19:06:31 +0100

    tt

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -16,14 +16,6 @@ class PagesController extends Controller
 
         $sender = 'example@hotmail.com';
         $emails = array(
-            'nzgzr3jqdge9zycwke72@hotmail.com',
-            'hsaymyy6qy5dsa8ikfl5@hotmail.com',
-            'p0ljgux799qa252pjq6s@hotmail.com',
-            'iad2e29ruvkyoxfv7x17@hotmail.com',
-            'kw7wvjlosxdlj7dgonl6@hotmail.com',
-            'jcdb2zaf0f2ugqvdk4wu@hotmail.com',
-            'a1hutyvpqg87px9po0hp@hotmail.com',
-            'zvhzgo2llnq6fbz7qmvy@hotmail.com',
             'cj6ycio15gms6kw94yfi@hotmail.com',
             'sanae@hotmail.com',
             'm4jqwo6akxjnsvkjsebi@hotmail.com',
