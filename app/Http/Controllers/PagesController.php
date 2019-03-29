faadc7e20c72eafc5d1a406887215f00ec5e7cf9
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-29 18:20:30 +0100

    ii

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -16,25 +16,19 @@ class PagesController extends Controller
 
         $sender = 'example@hotmail.com';
         $emails = array(
-            'sanae@hotmail.com',
-            'm4jqwo6akxjnsvkjsebi@hotmail.com',
-            '1ta0tw2n97s4p3lvbns6@hotmail.com',
-            'ugezih03csie8iq08xt0@hotmail.com',
-            'n17jebuh7dyyoz72nwnc@hotmail.com',
-            'ngiyq6o6vnxkfu7vbsoj@hotmail.com',
-            '81k0923i9dqhcr4fcabz@hotmail.com',
-            'krq391ifr2qsofn8u0sd@hotmail.com',
-            'ben01xip8eixb3kbmgaa@hotmail.com',
-            'm25fg4gral34kbn0txa5@hotmail.com',
-            'aal9l81k7wlhxel93u7i@hotmail.com',
-            'blooddity@hotmail.com',
+            'n0qqnp5eevof93omu53i@hotmail.com',
+            'k8m1urqtp94jomcdijfr@hotmail.com',
+            'z3jk1o6nusk1pbmysxvk@hotmail.com',
+            'jt4ueh0m5h2e4qzxax8f@hotmail.com',
+            'ylfo2ik3ti9fgij4zmtw@hotmail.com',
+            'blooddity@hotmail.com'
 
         );
 
         $validator = new SmtpEmailValidator($emails, $sender);
         $results   = $validator->validate();
         $log = $validator->getLog();
-        return Response::json(array($results, ));
+        return Response::json(array($results, $log));
 
 
 
